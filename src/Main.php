56e0d7ef3d2d2f22050519e557b9cdd76b8e55db
FactionsPlus12 <factionplus12@gmail.com>
2017-12-03 16:47:26 -0500

    Add files via upload

---
+++ src/Main.php
@@ -0,0 +1,30 @@
+<?php
+namespace BasicCommands;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Player;
+use pocketmine\Command\Command;
+use pocketmine\Command\CommandSender;
+use pocketmine\entity\Effect;
+class Main extends PluginBase{
+	
+	public function onEnable(){
+		$this->getServer()->getLogger()->info("BasicCommands enabled!");
+	}
+	
+	public function onDisable(){
+		$this->getServer()->getLogger()->info("BasicCommands disabled!");
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+		
+		switch($cmd->getName()){
+			
+			case "clear":
+				if($sender instanceof Player){
+					$sender->getInventory()->clearAll();
+				}
+			
+		}
+		return true;
+	}
+}
